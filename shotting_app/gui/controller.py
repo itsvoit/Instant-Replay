3cdb19eccdca55791014c1c27b5cb633217b9ff1
skowron4 <adam.skowron44@gmail.com>
2023-06-20 01:36:11 +0200

    add controller, gui methods; and icons

--- shotting_app/gui/controller.py
+++ shotting_app/gui/controller.py
@@ -1,8 +1,32 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from gui import UiMainWindow
+import qdarkstyle
 
 
 class Controller:
     def __init__(self):
-        ...
+        app = QApplication(sys.argv)
+        app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+        self.view = UiMainWindow(self)
+        self.get_user_settings()
+        self.view.show()
+        sys.exit(app.exec())
+
+    def get_user_settings(self):
+        #najlepsza opcja byloby chyba zwracanie slownika <wartość: lista wartosci>,
+        # bo boxy i inne, przyjmuja liste stringow i moge dodac od razu te opcje
+        # no i pierwsza wartoscia jest jest opcja z .conf
+
+        return ... #todo zwraca slownik z ustawieniami
+
+    def get_default_settings(self):
+        return ... #todo zwraca default ustawienia
+
+    def save_settings(self, settings):
+        #todo zapisuje wartosc settings, wczesniej musi byc zrobione obliczanie ramu
+        # i wpisanie wartosci do settings['ram_ussage'], po tym zwraca settings['ram_ussage']
+        return ...
 
     def start_capture(self):
         ...
--- shotting_app/gui/gui.py
+++ shotting_app/gui/gui.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QFileDialog
 
 widget_back_ground = "background-color: #505F72"
 menu_button_style = '''
@@ -11,9 +12,154 @@ menu_button_style = '''
                     }'''
 
 
-class UiMainWindow(object):
-    def make_central_widget(self, main_window):
-        central_widget = QtWidgets.QWidget(main_window)
+class UiMainWindow(QMainWindow):
+
+    def __init__(self, controller):
+        super().__init__()
+        self.main_window.setObjectName("Screen Recorder")
+        self.main_window.resize(875, 612)
+        self.main_window.setMinimumSize(QtCore.QSize(875, 612))
+        self.main_window.setMaximumSize(QtCore.QSize(875, 612))
+
+        self.controller = controller
+        self.central_widget = self.make_central_widget()
+        self.full_menu_widget = self.make_menu_widget()
+        self.menu_vertica_layout = self.make_menu_layout()
+        self.option_v_layout = self.make_vertical_layaut("verticalLayout_4", self.full_menu_widget)
+
+        self.option_button = self.make_menu_button("option_button", ":/icons/settings_cogwheel_options_icon_149387.png")
+        self.option_button.clicked.connect(self.select_option_widget())
+
+        self.menu_vertica_layout.addWidget(self.option_button)
+
+        self.video_button = self.make_menu_button("video_button",
+                                                  ":/icons/movie-symbol-of-video-camera_icon-icons.com_72981.png")
+
+        self.menu_vertica_layout.addWidget(self.video_button)
+
+        self.editor_button = self.make_menu_button("editor_button",
+                                                   ":/icons/iconfinder-videoeditingeditorslicecrop-3993845_112640.png")
+
+        self.menu_vertica_layout.addWidget(self.editor_button)
+
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.menu_vertica_layout.addItem(spacerItem)
+
+        self.option_v_layout.addLayout(self.menu_vertica_layout)
+
+        self.exit_button = self.make_menu_button("exit_button", ":/icons/powercircleandlinesymbol_118369.png")
+
+        self.option_v_layout.addWidget(self.exit_button)
+        self.exit_button.clicked.connect(self.close_app)
+
+        self.main_widget = self.make_main_widget()
+        self.main_stacked_widget = self.make_stacked_widget()
+
+        self.option = QtWidgets.QWidget()
+        self.option.setObjectName("option")
+
+        self.resolution_combo_box = self.make_combo_box("resolution_combo_box", (270, 30))
+        self.FPS_combo_box = self.make_combo_box('FPS_combo_box', (270, 80))
+        self.codec_combo_box = self.make_combo_box('codec_combo_box', (270, 130))
+        self.display_combo_box = self.make_combo_box('display_combo_box', (270, 530))
+
+        self.s_storage_line = self.make_storage_line('s_storage_line', (270, 480))
+        self.v_storage_line = self.make_storage_line("v_storage_line", (270, 430))
+
+        self.quality_slider = self.make_slider("quality_slider", (270, 330), 0, 100)
+
+        self.sounds_button = self.make_check_button("sounds_button", (350, 280))
+
+        self.layout_widget_labels = self.make_layout_widget_for_label()
+
+        self.menu_label_vertica_layout = self.make_vertical_layaut("menu_label_vertica_layout",
+                                                                   self.layout_widget_labels)
+
+        self.resolution_label = self.make_label("resolution_label", self.layout_widget_labels)
+        self.menu_label_vertica_layout.addWidget(self.resolution_label)
+
+        self.FPS_label = self.make_label("FPS_label", self.layout_widget_labels)
+        self.menu_label_vertica_layout.addWidget(self.FPS_label)
+
+        self.Codec_label = self.make_label("codec_label", self.layout_widget_labels)
+        self.menu_label_vertica_layout.addWidget(self.Codec_label)
+
+        self.v_hotkey = self.make_label("v_hotkey", self.layout_widget_labels)
+        self.menu_label_vertica_layout.addWidget(self.v_hotkey)
+
+        self.s_hotkey = self.make_label("s_hotkey", self.layout_widget_labels)
+        self.menu_label_vertica_layout.addWidget(self.s_hotkey)
+
+        self.sound_label = self.make_label("sound_label", self.layout_widget_labels)
+        self.menu_label_vertica_layout.addWidget(self.sound_label)
+
+        self.quality_label = self.make_label("quality_label", self.layout_widget_labels)
+        self.menu_label_vertica_layout.addWidget(self.quality_label)
+
+        self.duration_label = self.make_label("duration_label", self.layout_widget_labels)
+        self.menu_label_vertica_layout.addWidget(self.duration_label)
+
+        self.v_storage_label = self.make_label("v_storage_label", self.layout_widget_labels)
+        self.menu_label_vertica_layout.addWidget(self.v_storage_label)
+
+        self.s_storage_label = self.make_label("s_storage_label", self.layout_widget_labels)
+        self.menu_label_vertica_layout.addWidget(self.s_storage_label)
+
+        self.display_label = self.make_label("display_label", self.layout_widget_labels)
+        self.menu_label_vertica_layout.addWidget(self.display_label)
+
+        self.v_storage_browse = self.make_button("v_storage_button", (470, 430))
+        self.v_storage_browse.clicked.connect(self.browse_v_storage)
+
+        self.s_storage_browse = self.make_button("s_storage_button", (470, 480))
+        self.s_storage_browse.clicked.connect(self.browse_s_storage)
+
+        self.duration_display = self.make_lcd_display("duration_display", (470, 330), 91, 32)
+
+        self.reset_button = self.make_button("reset_button", (470, 570))
+        self.reset_button.clicked.connect(self.restart_settings)
+
+        self.save_button = self.make_button("save_button", (590, 570))
+        self.save_button = self.save_button.clicked.connect(self.save_settings)
+
+        self.duration_horizontal_slider = self.make_horizontal_slider("duration_slider", (270, 380), 10, 120)
+
+        self.v_dur_display = self.make_lcd_display("v_dur_display", (470, 380), 91, 32)
+
+        self.video_hotkey = self.make_line("video_path", (270, 180))
+
+        self.screen_hotkey = self.make_line("screen_path", (270, 230))
+
+        self.ram_label = self.make_label("ram_req_label", self.option)
+        self.ram_label.setGeometry(QtCore.QRect(480, 60, 201, 41))
+
+        self.ram_display = self.make_lcd_display("ram_display", (480, 100), 210, 42)
+
+        self.main_stacked_widget.addWidget(self.option)
+
+        self.video = QtWidgets.QWidget()
+        self.video.setObjectName("video")
+
+        self.main_stacked_widget.addWidget(self.video)
+
+        self.video_editor = QtWidgets.QWidget()
+        self.video_editor.setObjectName("video_editor")
+
+        self.main_stacked_widget.addWidget(self.video_editor)
+        self.main_window.setCentralWidget(self.central_widget)
+
+        self.retranslateUi()
+
+        self.main_stacked_widget.setCurrentIndex(0)
+
+        self.quality_slider.valueChanged['int'].connect(self.duration_display.display)
+
+        self.duration_horizontal_slider.valueChanged['int'].connect(self.v_dur_display.display)
+
+        QtCore.QMetaObject.connectSlotsByName(self.main_window)
+
+    def make_central_widget(self):
+        central_widget = QtWidgets.QWidget(self.main_window)
         central_widget.setObjectName("central_widget")
 
         return central_widget
@@ -178,148 +324,66 @@ class UiMainWindow(object):
 
         return path
 
-    def make_option_widget(self):
-        self.option = QtWidgets.QWidget()
-        self.option.setObjectName("option")
+    def close_app(self):
+        self.main_winow.close()
 
-        self.resolution_combo_box = self.make_combo_box("resolution_combo_box", (270, 30))
-        self.FPS_combo_box = self.make_combo_box('FPS_combo_box', (270, 80))
-        self.codec_combo_box = self.make_combo_box('codec_combo_box', (270, 130))
-        self.display_combo_box = self.make_combo_box('display_combo_box', (270, 530))
-
-        self.s_storage_line = self.make_storage_line('s_storage_line', (270, 480))
-        self.v_storage_line = self.make_storage_line("v_storage_line", (270, 430))
-
-        self.duration_slider = self.make_slider("duration_slider", (270, 330), 0, 100)
-
-        self.sounds_button = self.make_check_button("sounds_button", (350, 280))
-
-        self.layout_widget_labels = self.make_layout_widget_for_label()
-
-        self.menu_label_vertica_layout = self.make_vertical_layaut("menu_label_vertica_layout",
-                                                                   self.layout_widget_labels)
-
-        self.resolution_label = self.make_label("resolution_label", self.layout_widget_labels)
-        self.menu_label_vertica_layout.addWidget(self.resolution_label)
-
-        self.FPS_label = self.make_label("FPS_label", self.layout_widget_labels)
-        self.menu_label_vertica_layout.addWidget(self.FPS_label)
-
-        self.Codec_label = self.make_label("codec_label", self.layout_widget_labels)
-        self.menu_label_vertica_layout.addWidget(self.Codec_label)
-
-        self.v_hotkey = self.make_label("v_hotkey", self.layout_widget_labels)
-        self.menu_label_vertica_layout.addWidget(self.v_hotkey)
-
-        self.s_hotkey = self.make_label("s_hotkey", self.layout_widget_labels)
-        self.menu_label_vertica_layout.addWidget(self.s_hotkey)
-
-        self.sound_label = self.make_label("sound_label", self.layout_widget_labels)
-        self.menu_label_vertica_layout.addWidget(self.sound_label)
-
-        self.quality_label = self.make_label("quality_label", self.layout_widget_labels)
-        self.menu_label_vertica_layout.addWidget(self.quality_label)
-
-        self.duration_label = self.make_label("duration_label", self.layout_widget_labels)
-        self.menu_label_vertica_layout.addWidget(self.duration_label)
-
-        self.v_storage_label = self.make_label("v_storage_label", self.layout_widget_labels)
-        self.menu_label_vertica_layout.addWidget(self.v_storage_label)
-
-        self.s_storage_label = self.make_label("s_storage_label", self.layout_widget_labels)
-        self.menu_label_vertica_layout.addWidget(self.s_storage_label)
-
-        self.display_label = self.make_label("display_label", self.layout_widget_labels)
-        self.menu_label_vertica_layout.addWidget(self.display_label)
-
-        self.v_storage_browse = self.make_button("v_storage_button", (470, 430))
-
-        self.s_storage_browse = self.make_button("s_storage_button", (470, 480))
-
-        self.duration_display = self.make_lcd_display("duration_display", (470, 330), 91, 32)
-
-        self.reset_button = self.make_button("reset_button", (470, 570))
-
-        self.save_button = self.make_button("save_button", (590, 570))
-
-        self.duration_horizontal_slider = self.make_horizontal_slider("horizontalSlider", (270, 380), 10, 120)
-
-        self.v_dur_display = self.make_lcd_display("v_dur_display", (470, 380), 91, 32)
-
-        self.video_path = self.make_line("video_path", (270, 180))
-
-        self.screen_path = self.make_line("screen_path", (270, 230))
-
-        self.ram_label = self.make_label("ram_req_label", self.option)
-        self.ram_label.setGeometry(QtCore.QRect(480, 60, 201, 41))
-
-        self.ram_display = self.make_lcd_display("ram_display", (480, 100), 210, 42)
-
-        self.main_stacked_widget.addWidget(self.option)
-
-    def setupUi(self, main_window):
-        main_window.setObjectName("SchoolSchooter47")
-        main_window.resize(875, 612)
-        main_window.setMinimumSize(QtCore.QSize(875, 612))
-        main_window.setMaximumSize(QtCore.QSize(875, 612))
-
-        self.central_widget = self.make_central_widget(main_window)
-        self.full_menu_widget = self.make_menu_widget()
-        self.menu_vertica_layout = self.make_menu_layout()
-        self.option_v_layout = self.make_vertical_layaut("verticalLayout_4", self.full_menu_widget)
-
-        self.option_button = self.make_menu_button("option_button", ":/icon/settings_cogwheel_options_icon_149387.png")
-
-        self.menu_vertica_layout.addWidget(self.option_button)
-
-        self.video_button = self.make_menu_button("video_button",
-                                                  ":/icon/movie-symbol-of-video-camera_icon-icons.com_72981.png")
-
-        self.menu_vertica_layout.addWidget(self.video_button)
-
-        self.editor_button = self.make_menu_button("editor_button",
-                                                   ":/icon/iconfinder-videoeditingeditorslicecrop-3993845_112640.png")
-
-        self.menu_vertica_layout.addWidget(self.editor_button)
-
-        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.menu_vertica_layout.addItem(spacerItem)
-
-        self.option_v_layout.addLayout(self.menu_vertica_layout)
-
-        self.exit_button = self.make_menu_button("exit_button", ":/icon/powercircleandlinesymbol_118369.png")
-
-        self.option_v_layout.addWidget(self.exit_button)
-
-        self.main_widget = self.make_main_widget()
-        self.main_stacked_widget = self.make_stacked_widget()
-
-        self.make_option_widget()
-
-        self.video = QtWidgets.QWidget()
-        self.video.setObjectName("video")
-
-        self.main_stacked_widget.addWidget(self.video)
-
-        self.video_editor = QtWidgets.QWidget()
-        self.video_editor.setObjectName("video_editor")
-
-        self.main_stacked_widget.addWidget(self.video_editor)
-        main_window.setCentralWidget(self.central_widget)
-
-        self.retranslateUi(main_window)
-
+    def select_option_widget(self):
         self.main_stacked_widget.setCurrentIndex(0)
 
-        self.duration_slider.valueChanged['int'].connect(self.duration_display.display)
+    def show_user_settings(self):
+        self.show_settings(self.controller.get_user_settings())
 
-        self.duration_horizontal_slider.valueChanged['int'].connect(self.v_dur_display.display)
+    def show_settings(self, settings):
+        self.resolution_combo_box.addItems(settings['resolution'])
+        self.resolution_combo_box.setCurrentIndex(0)
+        self.FPS_combo_box.addItems(settings['fps'])
+        self.FPS_combo_box.setCurrentIndex(0)
+        self.codec_combo_box.addItems(settings['codec'])
+        self.codec_combo_box.setCurrentIndex(0)
+        self.display_combo_box.addItems(settings['display'])
+        self.display_combo_box.setCurrentIndex(0)
 
-        QtCore.QMetaObject.connectSlotsByName(main_window)
+        self.video_hotkey.setText(settings['video_hotkey'])
+        self.screen_hotkey.setText(settings['screen_hotkey'])
+        self.sounds_button.setChecked(settings['save_sound'])
+        self.quality_slider.setValue(settings['quality'])
+        self.duration_horizontal_slider.setValue(settings['duration'])
+        self.v_storage_line.setText(settings['video_path'])
+        self.s_storage_line.setText(settings['screen_path'])
 
-    def retranslateUi(self, main_window):
+        self.ram_display.display(settings['ram_ussage'])
+
+    def restart_settings(self):
+        self.show_settings(self.controller.get_default_settings())
+
+    def save_settings(self):
+        new_settings = {'resolution': self.resolution_combo_box.currentText(),
+                        'fps': self.FPS_combo_box.currentText(),
+                        'codec': self.codec_combo_box.currentText(),
+                        'display': self.display_combo_box.currentText(),
+                        'video_hotkey': self.video_hotkey.text(),
+                        'screen_hotkey': self.screen_hotkey.text(),
+                        'save_sound': self.sounds_button.isChecked(),
+                        'quality': self.quality_slider.value(),
+                        'duration': self.duration_horizontal_slider.value(),
+                        'video_path': self.v_storage_line.text(),
+                        'screen_path': self.s_storage_line.text(),
+                        'ram_ussage': 0}
+        ram_ussage = self.controller.save_settings(new_settings)
+        new_settings['ram_ussage'] = ram_ussage
+        self.show_settings(new_settings)
+
+    def browse_v_storage(self):
+        folder = QFileDialog.getExistingDirectory(QMainWindow(), "Wybierz folder")
+        self.v_storage_line.setText(folder)
+
+    def browse_s_storage(self):
+        folder = QFileDialog.getExistingDirectory(QMainWindow(), "Wybierz folder")
+        self.s_storage_line.setText(folder)
+
+    def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
-        main_window.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.main_window.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.option_button.setText(_translate("MainWindow", "Option"))
         self.video_button.setText(_translate("MainWindow", "Video"))
         self.editor_button.setText(_translate("MainWindow", "Video editor"))
