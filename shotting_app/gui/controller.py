11f6fab76bfa5ca8141839a14ddffe838e810537
skowron4 <adam.skowron44@gmail.com>
2023-06-20 18:27:37 +0200

    fix gui

--- shotting_app/gui/controller.py
+++ shotting_app/gui/controller.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from gui import UiMainWindow
+from shotting_app.gui.gui import UiMainWindow
 import qdarkstyle
 
 
@@ -14,19 +14,52 @@ class Controller:
         sys.exit(app.exec())
 
     def get_user_settings(self):
-        #najlepsza opcja byloby chyba zwracanie slownika <wartość: lista wartosci>,
+        # najlepsza opcja byloby chyba zwracanie slownika <wartość: lista wartosci>,
         # bo boxy i inne, przyjmuja liste stringow i moge dodac od razu te opcje
-        # no i pierwsza wartoscia jest jest opcja z .conf
+        # no i pierwsza wartoscia jest opcja z .conf
 
-        return ... #todo zwraca slownik z ustawieniami
+        return {'resolution': ['1920x1080'],
+                'fps': ['15', '30','50', '60'],
+                'codec': ['H.264', 'H.265'],
+                'display': ['Screen 1', 'Screen 2'],
+                'video_hotkey': 'Ctrl+X',
+                'screen_hotkey': 'Ctrl+Y',
+                'save_sound': False,
+                'quality': 85,
+                'duration': 30,
+                'video_path': './Wideo',
+                'screen_path': './ScreenShoots',
+                'ram_ussage': 400}
 
     def get_default_settings(self):
-        return ... #todo zwraca default ustawienia
-
+        return {'resolution': ['1920x1080'],
+                'fps': ['15', '30', '50', '60'],
+                'codec': ['H.264', 'H.265'],
+                'display': ['Screen 1', 'Screen 2'],
+                'video_hotkey': 'Ctrl+X',
+                'screen_hotkey': 'Ctrl+Y',
+                'save_sound': False,
+                'quality': 85,
+                'duration': 30,
+                'video_path': './Wideo',
+                'screen_path': './ScreenShoots',
+                'ram_ussage': 400}
+    
     def save_settings(self, settings):
         #todo zapisuje wartosc settings, wczesniej musi byc zrobione obliczanie ramu
         # i wpisanie wartosci do settings['ram_ussage'], po tym zwraca settings['ram_ussage']
-        return ...
+        return {'resolution': ['1920x1080'],
+                'fps': ['15', '30', '50', '60'],
+                'codec': ['H.264', 'H.265'],
+                'display': ['Screen 1', 'Screen 2'],
+                'video_hotkey': 'Ctrl+X',
+                'screen_hotkey': 'Ctrl+Y',
+                'save_sound': False,
+                'quality': 85,
+                'duration': 30,
+                'video_path': './Wideo',
+                'screen_path': './ScreenShoots',
+                'ram_ussage': 400}
 
     def start_capture(self):
         ...
